74ed39b509bf09ca6e2501a64c01975f9f02e29d
Andrei <144918860+Andrey123Andrey@users.noreply.github.com>
2024-09-16 21:33:50 +0300

    Add files via upload

---
+++ Zadanie14Tarasenkov/Zadanie14Tarasenkov/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Zadanie14Tarasenkov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelColor = new System.Windows.Forms.Label();
+            this.comboBoxColors = new System.Windows.Forms.ComboBox();
+            this.buttonInput = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelColor
+            // 
+            this.labelColor.AutoSize = true;
+            this.labelColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelColor.Location = new System.Drawing.Point(223, 78);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(429, 38);
+            this.labelColor.TabIndex = 0;
+            this.labelColor.Text = "Выберете цвет из списка";
+            this.labelColor.Click += new System.EventHandler(this.labelColor_Click);
+            // 
+            // comboBoxColors
+            // 
+            this.comboBoxColors.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxColors.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxColors.FormattingEnabled = true;
+            this.comboBoxColors.Location = new System.Drawing.Point(229, 140);
+            this.comboBoxColors.Name = "comboBoxColors";
+            this.comboBoxColors.Size = new System.Drawing.Size(248, 39);
+            this.comboBoxColors.TabIndex = 1;
+            this.comboBoxColors.Visible = false;
+            this.comboBoxColors.SelectedIndexChanged += new System.EventHandler(this.comboBoxColors_SelectedIndexChanged);
+            // 
+            // buttonInput
+            // 
+            this.buttonInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonInput.Location = new System.Drawing.Point(48, 330);
+            this.buttonInput.Name = "buttonInput";
+            this.buttonInput.Size = new System.Drawing.Size(221, 62);
+            this.buttonInput.TabIndex = 2;
+            this.buttonInput.Text = "Ввод";
+            this.buttonInput.UseVisualStyleBackColor = true;
+            this.buttonInput.Click += new System.EventHandler(this.buttonInput_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClear.Location = new System.Drawing.Point(537, 330);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(221, 62);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonInput);
+            this.Controls.Add(this.comboBoxColors);
+            this.Controls.Add(this.labelColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.ComboBox comboBoxColors;
+        private System.Windows.Forms.Button buttonInput;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ Zadanie14Tarasenkov/Zadanie14Tarasenkov/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zadanie14Tarasenkov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Подключаем обработчики событий
+            this.Load += new System.EventHandler(this.Form1_Load);
+            buttonInput.Click += new System.EventHandler(this.buttonInput_Click);
+            comboBoxColors.SelectedIndexChanged += new System.EventHandler(this.comboBoxColors_SelectedIndexChanged);
+            buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Заполнение комбинированного списка цветами
+            AddColorToComboBox("Красный");
+            AddColorToComboBox("Зелёный");
+            AddColorToComboBox("Синий");
+            AddColorToComboBox("Фиолетовый");
+            AddColorToComboBox("Оранжевый");
+            AddColorToComboBox("Белый");
+        }
+
+        private void AddColorToComboBox(string color)
+        {
+            // Проверка, существует ли цвет в комбинированном списке
+            if (!comboBoxColors.Items.Contains(color))
+            {
+                comboBoxColors.Items.Add(color);
+            }
+        }
+
+        private void buttonInput_Click(object sender, EventArgs e)
+        {
+            // Показываем комбинированный список
+            comboBoxColors.Visible = true;
+        }
+
+        private void comboBoxColors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Переносим выбранный цвет на ярлык
+            labelColor.Text = comboBoxColors.SelectedItem.ToString();
+
+            // Скрываем комбинированный список
+            comboBoxColors.Visible = false;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // Очищаем текст ярлыка
+            labelColor.Text = string.Empty;
+        }
+
+        private void labelColor_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
