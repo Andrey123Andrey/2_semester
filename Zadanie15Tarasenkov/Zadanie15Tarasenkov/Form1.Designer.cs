58e240470a88a3c4feb01ebe2af3adc972c5d33c
Andrei <144918860+Andrey123Andrey@users.noreply.github.com>
2024-10-02 03:34:24 +0300

    Add files via upload

---
+++ Zadanie15Tarasenkov/Zadanie15Tarasenkov/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Zadanie15_2_Tarasenkov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.monthCalendar2 = new System.Windows.Forms.MonthCalendar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(107, 148);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // monthCalendar2
+            // 
+            this.monthCalendar2.Location = new System.Drawing.Point(515, 148);
+            this.monthCalendar2.Name = "monthCalendar2";
+            this.monthCalendar2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(107, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(192, 30);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Выберете год";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(438, 106);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(373, 30);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "В календере выделен день пасхи";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 496);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.monthCalendar2);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.MonthCalendar monthCalendar2;
+        private System.Windows.Forms.TextBox textBox2;
+        protected System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Zadanie15Tarasenkov/Zadanie15Tarasenkov/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace Zadanie15_2_Tarasenkov
+{
+    public partial class Form1 : Form
+    {
+        private MonthCalendar Calendar_of_year;
+        private MonthCalendar Calendar_of_easters;
+        private int selectedYear;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Calendar_of_year = monthCalendar1;
+            Calendar_of_easters = monthCalendar2;
+            selectedYear = DateTime.Now.Year;
+            Calendar_of_year.ShowToday = false;
+            Calendar_of_year.MaxSelectionCount = 1;
+            Calendar_of_easters.MaxSelectionCount = 1;
+            Calendar_of_year.DateChanged += Calendar_of_year_DateChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Задание №15. Тарасенков А.Д. Календарь Пасхи";
+        }
+
+        private void Calendar_of_year_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            selectedYear = Calendar_of_year.SelectionRange.Start.Year; Calendar_of_easters.RemoveAllBoldedDates();
+            string[] weekendDays = File.ReadAllLines("Days_of_Easters.txt");
+            foreach (string day in weekendDays)
+            {
+                DateTime date = DateTime.ParseExact(day, "dd.MM.yyyy", null);
+
+                if (date.Year == selectedYear)
+                {
+                    if (date.DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        Calendar_of_easters.AddBoldedDate(date);
+                    }
+                }
+            }
+            Calendar_of_easters.SetDate(new DateTime(selectedYear, 1, 1));
+        }
+    }
+}
